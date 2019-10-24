5203a441fb0c4c3eacda019daea55485c9da8c8c
TEST <fazli.temizel@gmail.com>
2019-10-24 21:50:07 +0300

    v3
    Son Güncellemeler ve PWA eklendi

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# fazli.github.io
-
-Offical Portfolio Pages
-
-----
-
-Design by fazlio.github.io
-Code by berkpw.github.io
+# berk.github.io
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,78 @@
-const Foo = { template: '<div>foo</div>' };
-const Bar = { template: '<div>bar</div>' };
-
 const router = new VueRouter({
   mode: 'history',
   base: '/Portfolio/',
-  routes: [
-    { path: '/foo', component: Foo },
-    { path: '/bar', component: Bar }
-  ]
+  routes: []
 });
-
 const app = new Vue({
   router,
   data() {
     return {
       load: false,
-      config: ""
+      config: "",
+      view: false,
+      viewActive: 0,
+      pageTop: 0
     }
   },
+  methods: {
+    openItem: function(item, url = true) {
+      this.pageTop = window.pageYOffset || document.documentElement.scrollTop;
+      window.scrollTo(0,0);
+      if (url) this.$router.push({query: {item: this.url(item.title)}});
+      this.view = item;
+      this.viewActive = 0;
+    },
+    changeActive: function (action) {
+      window.scrollTo(0,0);
+      if (action === 'left') {
+        if (this.viewActive === 0) return false;
+        this.viewActive -= 1
+      }
+      if (action === 'right') {
+        if (this.viewActive+1 === this.view.images.length) return false;
+        this.viewActive += 1
+      }
+    },
+    close: function () {
+      window.scrollTo(0, this.pageTop);
+      this.view = false;
+      this.$router.push({query: {item: null}});
+    },
+    url: function (string) {
+      const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧıîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
+      const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------';
+      const p = new RegExp(a.split('').join('|'), 'g');
+
+      return string.toString().toLowerCase().replace(/\s+/g, '-').replace(p, c => b.charAt(a.indexOf(c))).replace(/&/g, '-and-').replace(/[^\w\-]+/g, '').replace(/\-\-+/g, '-').replace(/^-+/, '').replace(/-+$/, '')
+    }
+  },
+  mounted() {
+    let that = this;
+    $(document).bind('keyup', function(event) {
+      if (event.keyCode === 27) that.close();
+      if (event.keyCode === 37) that.changeActive('left');
+      if (event.keyCode === 39) that.changeActive('right');
+    });
+  },
   beforeCreate() {
-    axios.get('config.json').then(r => {
+    axios.get('./config.json').then(r => {
       this.config = JSON.parse(JSON.stringify(r.data));
     }).then(() => {
-      document.title = this.config.site.title;
-      $('meta[name="description"]').attr("content", this.config.site.desc);
-      $('link[rel="shortcut icon"]').attr("href", this.config.site.favicon);
+      document.title = this.config.name;
+      $('meta[name="description"]').attr("content", this.config.description);
+      $('link[rel="shortcut icon"], link[rel="apple-touch-icon"]').attr("href", this.config.icons[0].src);
+      if (this.$route.query.item) {
+        let isItem = this.config.portfolio.find(e => this.url(e.title) === this.$route.query.item);
+        if (isItem) {
+          this.openItem(isItem, false);
+        }
+      }
       this.load = true;
     });
   }
 }).$mount('#app');
+
+// PWA Code
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(() => console.log("[SW] Is activated."));
+}
